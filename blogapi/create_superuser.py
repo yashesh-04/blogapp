f0b0d4ddfd00a8dd7011b69e15a05e1018825b8a
yashesh-04 <111443277+yashesh-04@users.noreply.github.com>
2026-07-07 12:38:29 +0530

    Change in structure of the create_superuser.py

--- blogapi/create_superuser.py
+++ blogapi/create_superuser.py
@@ -1,10 +1,10 @@
 import os
 import django
-from django.contrib.auth.models import User
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogapi.settings')
 django.setup()
 
+from django.contrib.auth.models import User
 
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
