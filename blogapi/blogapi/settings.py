8570265969dd4fe773e13b014c9aafaa6a9f43c8
Yashesh Shah <yasheshshah04@gmail.com>
2026-07-07 12:20:36 +0530

    Added Procfile, requirements, whitenoise

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)n7u&ks_l$v-vz%pylb+5l-3z+#zcgc-2k#n@e3&*+8wiq10i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
